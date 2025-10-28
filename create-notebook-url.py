76f8f91a006253fcc050864fbdc37a1158f315c8
sg-prog1 <gx7gydgxq5@privaterelay.appleid.com>
2025-10-28 12:37:49 +0200

    Create url to notebook script

---
+++ create-notebook-url.py
@@ -0,0 +1,35 @@
+import boto3
+
+AWS_ACCESS_KEY_ID = "AKIARJ5AUHRXZCKQZ5WS"
+AWS_SECRET_ACCESS_KEY = "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"
+REGION = "us-east-1"
+
+NOTEBOOK_NAME = "BiasAnalysis"
+# DOMAIN_ID = "d-xxxxxxxxxxxx"
+# USER_PROFILE = "user-profile-name"
+
+sm = boto3.client(
+    "sagemaker",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION,
+)
+
+def presign_notebook(name, duration=3600):
+    r = sm.create_presigned_notebook_instance_url(
+        NotebookInstanceName=name,
+        SessionExpirationDurationInSeconds=duration
+    )
+    print(r["AuthorizedUrl"])
+
+def presign_studio(domain_id, user_profile, duration=3600):
+    r = sm.create_presigned_domain_url(
+        DomainId=domain_id,
+        UserProfileName=user_profile,
+        SessionExpirationDurationInSeconds=duration
+    )
+    print(r["AuthorizedUrl"])
+
+if __name__ == "__main__":
+    presign_notebook(NOTEBOOK_NAME)
+    # presign_studio(DOMAIN_ID, USER_PROFILE)
